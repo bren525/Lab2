7ac9aa3d51b8c69a921b049f994adca82ab78eb0
Brendan <brendan@BrenCompRobo.(none)>
2015-03-13 06:23:02 -0400

    default map working side bar prettier

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -52,7 +52,7 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 
 			place.$save(function (response) {
 				$scope.place = response;
-				$scope.sideBarItems = $scope.place.playlist;
+				$scope.sidebarItems = $scope.place.playlist;
 			}, function (errorResponse){
 				$scope.error = errorResponse.data.message;
 			});
@@ -69,7 +69,7 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 				} else {
 					$scope.place = match;
 					console.log("place",$scope.place);
-					$scope.sideBarItems = $scope.place.playlist;
+					$scope.sidebarItems = $scope.place.playlist;
 				};
 			});
 		};
@@ -93,6 +93,8 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 
   			searchBox = new google.maps.places.SearchBox((input));
 
+  			$scope.mode = "playing";
+
   			function createPlaceMarker(place) {
 				var marker = new google.maps.Marker({
 				    map: map,
@@ -100,14 +102,10 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 				    position: place.geometry.location
 			 	});
 
-			 	infowindow.close();
-
 			 	google.maps.event.addListener(marker, 'click', function() {
 			 		infowindow.setContent("<div class=\"infowindow\"><h5>"+ place.name +"</h5>"+place.formatted_address+"</div>");
 			 		infowindow.open(map, marker);
 			 		checkPlaceInBase({title:place.name,placeId:place.place_id});
-		    		/*map.setZoom(16);
-		    		map.panTo(marker.getPosition());*/
 	  			});
 
 				return {title:place.name, placeId:place.place_id, marker:marker};
@@ -138,7 +136,27 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 			    var searchBounds = map.getBounds();
 			    searchBox.setBounds(searchBounds);
 			});
-			
+
+			service.nearbySearch({
+  				location: map.getCenter(),
+    			radius: '250'
+    			,types: ["accounting","airport","amusement_park","aquarium","art_gallery","atm","bakery","bank","bar","beauty_salon","bicycle_store","book_store","bowling_alley","bus_station","cafe","campground","car_dealer","car_rental","car_repair","car_wash","casino","cemetery","church","city_hall","clothing_store","convenience_store","courthouse","dentist","department_store","doctor","electrician","electronics_store","embassy","establishment","finance","fire_station","florist","food","funeral_home","furniture_store","gas_station","general_contractor","grocery_or_supermarket","gym","hair_care","hardware_store","health","hindu_temple","home_goods_store","hospital","insurance_agency","jewelry_store","laundry","lawyer","library","liquor_store","local_government_office","locksmith","lodging","meal_delivery","meal_takeaway","mosque","movie_rental","movie_theater","moving_company","museum","night_club","painter","park","parking","pet_store","pharmacy","physiotherapist","place_of_worship","plumber","police","post_office","real_estate_agency","restaurant","roofing_contractor","rv_park","school","shoe_store","shopping_mall","spa","stadium","storage","store","subway_station","synagogue","taxi_stand","train_station","travel_agency","university","veterinary_care","zoo"]
+  			}, 
+  			function(results){
+  				if(results.length > 0){
+  					service.getDetails({placeId: results[0].place_id}, function(details){
+  						var initMarker = createPlaceMarker(details);
+  						console.log("initial",details);
+  						foundPlaces.push(initMarker);
+  						infowindow.setContent("<div class=\"infowindow\"><h5>"+ details.name +"</h5>"+details.formatted_address+"</div>");
+  						infowindow.open(map,initMarker.marker);
+  						checkPlaceInBase({title:results[0].name,placeId:results[0].place_id});
+  					});
+  					
+  				}else{
+  					//checkPlaceInBase({title:"Franklin W. Olin College of Engineering",placeId:"ChIJu_Vj-z-B44kRW8PY7p4iOKA"});
+  				}
+  			});
 
 		}
 		if(navigator.geolocation){
