5dd784aeb4661bdfd35795f3010bfec0214c6677
Brendan <brendan@BrenCompRobo.(none)>
2015-03-13 07:56:29 -0400

    cleaning quotes

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -20,7 +20,7 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 						$scope.widget.load($scope.place.playlist[0].uri);
 					}
 				});
-				$scope.mode = "playing";
+				$scope.mode = 'playing';
 			});
 		}
 
@@ -28,24 +28,24 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 			console.log($scope.searchQuery);
 			$scope.soundcloud.fetchTracks($scope.searchQuery).then(function (tracks){
 				$scope.sidebarItems = tracks.slice(0,15);
-				$scope.mode = "searching";
+				$scope.mode = 'searching';
 			});
 		};
 
 		$scope.sidebarItemAction = function (sidebarItem) {
-			if ($scope.mode == "searching"){
+			if ($scope.mode == 'searching'){
 				$scope.place.playlist.push(sidebarItem);
 				$scope.updatePlace();
-				$scope.mode = "playing";
+				$scope.mode = 'playing';
 				$scope.sidebarItems = $scope.place.playlist;
-			} else if ($scope.mode == "playing") {
+			} else if ($scope.mode == 'playing') {
 				$scope.widget.load(sidebarItem.uri, {auto_play: true});
 			}
 			console.log(sidebarItem);
 		};
 
 		function playNext () {
-			console.log("playing next");
+			console.log('playing next');
 			var index = $scope.place.playlist.indexOf($scope.widget.getCurrentSound());
 			if (index < $scope.place.playlist.length){
 				$scope.widget.load($scope.place.playlist[index+1].uri, {auto_play: true})
@@ -84,7 +84,7 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 			$scope.places.get({
 				placeId: clickedPlace.placeId
 			},function (match) {
-				if (match.message == "Place not found") {
+				if (match.message == 'Place not found') {
 					createPlace(clickedPlace);
 
 				} else {
@@ -106,7 +106,7 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 			  center: new google.maps.LatLng(lat, lng),
 			  zoom: 13
 			};
-			map = new google.maps.Map(document.getElementById("map-canvas"),
+			map = new google.maps.Map(document.getElementById('map-canvas'),
 			mapOptions);
 
 			service = new google.maps.places.PlacesService(map);
@@ -115,7 +115,7 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 
   			searchBox = new google.maps.places.SearchBox((input));
 
-  			$scope.mode = "playing";
+  			$scope.mode = 'playing';
 
   			function createPlaceMarker(place) {
 				var marker = new google.maps.Marker({
@@ -125,7 +125,7 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 			 	});
 
 			 	google.maps.event.addListener(marker, 'click', function() {
-			 		infowindow.setContent("<div class=\"infowindow\"><h5>"+ place.name +"</h5>"+place.formatted_address+"</div>");
+			 		infowindow.setContent('<div class=\'infowindow\'><h5>'+ place.name +'</h5>'+place.formatted_address+'</div>');
 			 		infowindow.open(map, marker);
 			 		checkPlaceInBase({title:place.name,placeId:place.place_id});
 	  			});
@@ -161,22 +161,22 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 			service.nearbySearch({
   				location: map.getCenter(),
     			radius: '250'
-    			,types: ["accounting","airport","amusement_park","aquarium","art_gallery","atm","bakery","bank","bar","beauty_salon","bicycle_store","book_store","bowling_alley","bus_station","cafe","campground","car_dealer","car_rental","car_repair","car_wash","casino","cemetery","church","city_hall","clothing_store","convenience_store","courthouse","dentist","department_store","doctor","electrician","electronics_store","embassy","establishment","finance","fire_station","florist","food","funeral_home","furniture_store","gas_station","general_contractor","grocery_or_supermarket","gym","hair_care","hardware_store","health","hindu_temple","home_goods_store","hospital","insurance_agency","jewelry_store","laundry","lawyer","library","liquor_store","local_government_office","locksmith","lodging","meal_delivery","meal_takeaway","mosque","movie_rental","movie_theater","moving_company","museum","night_club","painter","park","parking","pet_store","pharmacy","physiotherapist","place_of_worship","plumber","police","post_office","real_estate_agency","restaurant","roofing_contractor","rv_park","school","shoe_store","shopping_mall","spa","stadium","storage","store","subway_station","synagogue","taxi_stand","train_station","travel_agency","university","veterinary_care","zoo"]
+    			,types: ['accounting','airport','amusement_park','aquarium','art_gallery','atm','bakery','bank','bar','beauty_salon','bicycle_store','book_store','bowling_alley','bus_station','cafe','campground','car_dealer','car_rental','car_repair','car_wash','casino','cemetery','church','city_hall','clothing_store','convenience_store','courthouse','dentist','department_store','doctor','electrician','electronics_store','embassy','establishment','finance','fire_station','florist','food','funeral_home','furniture_store','gas_station','general_contractor','grocery_or_supermarket','gym','hair_care','hardware_store','health','hindu_temple','home_goods_store','hospital','insurance_agency','jewelry_store','laundry','lawyer','library','liquor_store','local_government_office','locksmith','lodging','meal_delivery','meal_takeaway','mosque','movie_rental','movie_theater','moving_company','museum','night_club','painter','park','parking','pet_store','pharmacy','physiotherapist','place_of_worship','plumber','police','post_office','real_estate_agency','restaurant','roofing_contractor','rv_park','school','shoe_store','shopping_mall','spa','stadium','storage','store','subway_station','synagogue','taxi_stand','train_station','travel_agency','university','veterinary_care','zoo']
   			}, 
   			function(results){
   				if(results.length > 0){
   					service.getDetails({placeId: results[0].place_id}, function(details){
   						var initMarker = createPlaceMarker(details);
-  						console.log("initial",details);
+  						console.log('initial',details);
   						foundPlaces.push(initMarker);
-  						infowindow.setContent("<div class=\"infowindow\"><h5>"+ details.name +"</h5>"+details.formatted_address+"</div>");
+  						infowindow.setContent('<div class=\'infowindow\'><h5>'+ details.name +'</h5>'+details.formatted_address+'</div>');
   						infowindow.open(map,initMarker.marker);
   						checkPlaceInBase({title:results[0].name,placeId:results[0].place_id});
 						initWidget();
   					});
   					
   				}else{
-  					checkPlaceInBase({title:"Franklin W. Olin College of Engineering",placeId:"ChIJu_Vj-z-B44kRW8PY7p4iOKA"});
+  					checkPlaceInBase({title:'Franklin W. Olin College of Engineering',placeId:'ChIJu_Vj-z-B44kRW8PY7p4iOKA'});
 					initWidget();
   				}
   			});
